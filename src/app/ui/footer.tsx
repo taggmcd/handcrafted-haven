6f65e3a8931eb572dd633e7350f3f5c5940b2dd1
Tagg McDonald <taggmcd@gmail.com>
2024-10-09 19:19:31 -0600

    Fix Typo

--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -4,7 +4,7 @@
         <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center lg:px-8">
           <div className="mt-8 md:mt-0">
             <p className="text-center text-xs leading-5 text-gray-500">
-              &copy; 2024 Group 2 WDD430 BYU-I, Inc. All rights reserved.
+              &copy; 2024 Group 2 WDD430 BYU-I All rights reserved.
             </p>
           </div>
         </div>
