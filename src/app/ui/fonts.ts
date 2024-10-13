62bb19fbb4e7c851fe2fed17c0c95ca2b573a6e8
AmericoKutomi <americo@embra.com.br>
2024-10-12 22:20:33 -0300

    Corrections in package.json and fonts.ts

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -3,9 +3,4 @@ import { Roboto } from 'next/font/google';
 export const roboto = Roboto({
     weight: ['400', '700'],
     subsets: ['latin'],
-  });import { Roboto } from 'next/font/google';
- 
-export const roboto = Roboto({ 
-    subsets: ['latin'], 
-    weight: ['400', '700']
-});
+  });
