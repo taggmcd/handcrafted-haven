fbd875918fdb07169897daa2f73a0b89bdf78500
Tagg McDonald <taggmcd@gmail.com>
2024-10-28 19:42:54 -0600

    Adjust seller route

---
+++ src/app/sellers/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+// import SellerProfiles from '../../components/SellerProfiles';
+import { useEffect, useState } from 'react';
+
+
+const SellerProfilesPage = () => {
+  return (
+    <div>
+      <h1>Seller Profiles</h1>
+      {/* <SellerProfiles /> */}
+    </div>
+  );
+};
+
+export default SellerProfilesPage;
